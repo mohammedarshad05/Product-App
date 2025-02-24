9c8a0415e9e592cd0bbec6639cbd2ae48b4574ce
Mohammed Arshad <arshff005@gmail.com>
2025-02-24 14:13:08 +0530

    product app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ProductList from "./components/ProductList";
+function App() {
+  return (
+    <div>
+    <header>
+      <h1 >WELCOME TO MY STORE</h1>
+    </header>
+    <ProductList />
+    </div>
+  );    
+}
+export default App;
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from "react";
+
+interface Product {
+  id: number;
+  title: string;
+  thumbnail: string; // Updated from 'image' to 'thumbnail'
+  description: string;
+  category: string;
+  price: number;
+}
+
+const ProductList = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("https://dummyjson.com/products");
+
+      if (!response.ok) {
+        throw new Error("Error while fetching the products list");
+      }
+
+      const data = await response.json();
+      setProducts(data.products); 
+
+    } catch (error: any) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className="container">
+    <div className="row">
+      <h2>🏬 product Lists</h2>
+        {products.map((product) => (
+            <div className="col-md-4" key={product.id}>
+                <div className="card mb-4 ">
+                    <div className="card-body">
+                        <h5 className="card-title">{product.title}</h5>
+                        <img src={product.thumbnail} className="card-img-top" alt="..."/>
+                        <p className="card-text">{product.category}</p>
+                        <p className="card-text">{product.description}</p>
+                        <p className="card-text">{product.price}</p>
+                        {/* <Link className="btn btn-outline-primary" to={`/products/${recipe.id}`}>🍴{recipe.name}</Link> */}
+                        </div>
+                </div>
+            </div>
+        ))}
+    </div>
+</div>
+);
+}
+
+export default ProductList;
